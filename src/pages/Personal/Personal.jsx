3801d723cd03452fc64910665840801628ab1479
anhtuan-bui <anhtuan.bui@paxform.com>
2022-11-28 12:13:46 +1100

    change readme.md

---
+++ src/pages/Personal/Personal.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './Personal.scss';
+
+export default class Personal extends Component {
+  render() {
+    return (
+      <div>Personal</div>
+    )
+  }
+}
