2b98098cd45ffb0bf0ca0e3cf144d17b45b35db0
anhtuan-bui <anhtuan.bui@paxform.com>
2022-11-28 17:07:44 +1100

    add personal page

--- src/pages/Personal/Personal.jsx
+++ src/pages/Personal/Personal.jsx
@@ -7,12 +7,13 @@ import personelHero from '../../assets/images/personal-hero.svg'
 export default class Personal extends Component {
   render() {
     return (
-      <main>
+      <main className='personal'>
         <section className="hero">
                     <div className='container hero__wrapper'>
                         <div className='hero__content'>
-                            <h1 className='hero__title'>Forms<br /> without forms.</h1>
-                            <p className='hero__description'>Paxform allows users to securely save and manage their personal data for immediate reference, enabling them to easily sign documents and share information with others.</p>
+                          <p className='hero__name'>PERSONAL</p>
+                            <h1 className='hero__title'>Your Personal Information in One Place Fill Any Form, Anywhere</h1>
+                            <p className='hero__description'>Paxform’s function is simple: it completely fills any form, anytime. You can manage and save your personal data, allowing you to sign documents and send them to organisations with ease. No need to worry about security and privacy—Paxform fully encrypts your data to ensure your safety.</p>
                             <div className="hero__button">
                                 <Button text="See Plan" type="flat arrow" color="white" />
                                 <Button text="How it works" type="outline triangle-right" color="white" />
@@ -21,10 +22,9 @@ export default class Personal extends Component {
                         </div>
                         <div className="hero__image">
                             <img src={personelHero} alt="personal hero" className='hero__image-large' />
-                            {/* <CertificateIllustrator className='hero__image-ci' /> */}
                         </div>
                     </div>
-                    <div className="bottom_triangle"></div>
+                    <div className="bottom_triangle bottom_triangle--light-green"></div>
                 </section>
       </main>
     )
