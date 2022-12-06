c66c5dd24e64474e85f7a52597e5cc83c4986d13
Anh Tuan <anhtuan128@gmail.com>
2022-12-07 10:39:55 +1100

    push faq to live

--- src/components/ReadArticle/ReadArticle.jsx
+++ src/components/ReadArticle/ReadArticle.jsx
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
-import './ReadArticle.scss';
-import { Link } from 'react-router-dom';
-import {ReactComponent as ArrowRight} from '../../assets/images/arrow-right.svg';
+import React, { Component } from "react";
+import "./ReadArticle.scss";
+import { Link } from "react-router-dom";
+import { ReactComponent as ArrowRight } from "../../assets/images/arrow-right.svg";
 export default class ReadArticle extends Component {
-    render() {
-        return (
-            <Link className='read_article span--hover' to={this.props.to}>
-                <span >Read Article</span>
-                <ArrowRight />
-            </Link>
-        )
-    }
+  render() {
+    return (
+      <Link className="read_article span--hover" to={this.props.to}>
+        <span>Read Article</span>
+        <ArrowRight />
+      </Link>
+    );
+  }
 }
