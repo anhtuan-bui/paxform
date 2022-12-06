e8c440ca314c6dc318e0134486e12a0f041c0341
Anh Tuan <anhtuan128@gmail.com>
2022-12-06 22:59:27 +1100

    building resources

--- src/components/RelatedCard/RelatedCard.jsx
+++ src/components/RelatedCard/RelatedCard.jsx
@@ -7,24 +7,33 @@ export default class RelatedCard extends Component {
         return (
             <div className="related_card">
                 <div className="related_card__top">
-
-                    {this.props.image &&
-                        <div className="related_card__image">
+                    {this.props.image
+                        ? <div className="related_card__image">
                             <img src={this.props.image} alt="first story" />
                         </div>
+                        : ''
                     }
                     {
-                        this.props.title &&
-                        <h2 className='related_card__title'>First Story</h2>
+                        this.props.category ?
+                            <p className="related_card__category">Insight</p>
+                            : ''
                     }
                     {
-                        this.props.description && <p className="related_card__description">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour...</p>
+                        this.props.title ?
+                            <h2 className='related_card__title'>First Story</h2>
+                            : ''
+                    }
+                    {
+                        this.props.description
+                            ? <p className="related_card__description">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour...</p>
+                            : ''
                     }
                 </div>
                 <div className="related_card__bottom">
                     {
-                        this.props.readLink &&
-                        <ReadArticle to="related" />
+                        this.props.readLink
+                            ? <ReadArticle to="related" />
+                            : ''
                     }
                 </div>
             </div>
--- src/components/ResourceCard/ResourceCard.jsx
+++ src/components/ResourceCard/ResourceCard.jsx
@@ -8,13 +8,20 @@ import Button from '../Button/Button';
 export default class ResourceCard extends Component {
   render() {
     return (
-      <div className='resource_card'>
+      <div className={`resource_card ${this.props.className ? this.props.className : ''}`}>
+
         <div className="resource_card__background">
-            <img src={resourceCard1} alt="resource card 1" />
+          <img src={resourceCard1} alt="resource card" />
         </div>
-        <div className="resource_card__content">
-            <h2 className="resource_card__content-title">There are many variations of passages</h2>
-            <Button type="flat arrow" text="Read Article"/>
+
+        <div className={`resource_card__content-overlay ${this.props.overlay !== undefined ? `resource_card__content-overlay--${this.props.overlay}` : ''}`}></div>
+        <div className='resource_card__content'>
+
+          <h2 className="resource_card__content-title">There are many variations of passages</h2>
+          <div className="resource_card__content-button">
+            <Button type="flat arrow" text="Read Article" />
+
+          </div>
         </div>
       </div>
     )
