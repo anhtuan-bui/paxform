27ee468c17af66aa6b915aca6e7cb897e9498e39
Miri Kim <miri.kim@paxform.com>
2022-12-22 09:10:31 +1100

    pages name chanaged

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,8 @@ import { lazy, Suspense, useEffect } from "react";
 
 import { ApolloProvider } from "@apollo/client/react";
 import client from "./configurations/apollo";
-import GlobalPrivacyPolicy from "./pages/PrivacyPolicy/PrivacyPolicy";
-import SecurityPolicy from "./pages/LegalDetail/LegalDetail";
+import PrivacyPolicy from "./pages/PrivacyPolicy/PrivacyPolicy";
+import LegalDetail from "./pages/LegalDetail/LegalDetail";
 import getToken from "./lib/clientToken";
 
 const Resources = lazy(() => import("./pages/Resources/Resources"));
@@ -20,44 +20,40 @@ const Home = lazy(() => import("./pages/Home/Home"));
 const Personal = lazy(() => import("./pages/Personal/Personal"));
 const UseCases = lazy(() => import("./pages/UseCases/UseCases"));
 
-
 function App() {
-  const MINUTE_MS = 250000;
-  
-  getToken();
-  useEffect(() => {
-    const interval = setInterval(() => {
-      getToken();
-    }, MINUTE_MS);
-  
-    return () => clearInterval(interval);
-  }, [])
+	const MINUTE_MS = 250000;
 
-  return (
-    <ApolloProvider client={client}>
-      <BrowserRouter>
-        <Suspense>
-          <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route index element={<Home />} />
-              <Route path="personal" element={<Personal />} />
-              <Route path="faq" element={<FAQ />} />
+	getToken();
+	useEffect(() => {
+		const interval = setInterval(() => {
+			getToken();
+		}, MINUTE_MS);
 
-						<Route path="resources" element={<Resources />} />
-						<Route path="blogs" element={<Blogs />} />
-						<Route
-							path="global-privacy-policy"
-							element={<GlobalPrivacyPolicy />}
-						/>
-						<Route path="security-policy" element={<SecurityPolicy />} />
-						<Route path="not-found" element={<NotFound />} />
-						<Route path="usecases" element={<UseCases />} />
-						{/* <Route path="*" element={<NotFound />} /> */}
-					</Route>
-				</Routes>
-			</Suspense>
-		</BrowserRouter>
-    </ApolloProvider>
+		return () => clearInterval(interval);
+	}, []);
+
+	return (
+		<ApolloProvider client={client}>
+			<BrowserRouter>
+				<Suspense>
+					<Routes>
+						<Route path="/" element={<Layout />}>
+							<Route index element={<Home />} />
+							<Route path="personal" element={<Personal />} />
+							<Route path="faq" element={<FAQ />} />
+
+							<Route path="resources" element={<Resources />} />
+							<Route path="blogs" element={<Blogs />} />
+							<Route path="privacy-policy" element={<PrivacyPolicy />} />
+							<Route path="legal-detail" element={<LegalDetail />} />
+							<Route path="not-found" element={<NotFound />} />
+							<Route path="usecases" element={<UseCases />} />
+							{/* <Route path="*" element={<NotFound />} /> */}
+						</Route>
+					</Routes>
+				</Suspense>
+			</BrowserRouter>
+		</ApolloProvider>
 	);
 }
 
