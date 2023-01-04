9958d9462121749ecc2570d8a507a152065dcb57
Miri Kim <miri.kim@paxform.com>
2023-01-04 15:38:05 +1100

    contact us done

--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -7,60 +7,62 @@ export default class ContactUs extends Component {
 		return (
 			<main className="contact">
 				<section className="hero">
-					<div className="container hero__wrapper">
-						<div className="hero__content">
-							<p className="hero__name">Contact Us</p>
-							<h1 className="hero__title">We’d love to hear from you</h1>
-							<p className="hero__description">
-								If you have any questions about Paxdorm please contact us in
-								form beside and we will respond immediately.
-							</p>
-						</div>
-						<div className="form__box">
-							<form>
-								<label>
-									<strong>Full name</strong>
-									<input type="text" placeholder="John Doe" name="" />
-								</label>
-								<label>
-									<strong>Email</strong>
-									<input type="text" placeholder="jondoe@email.com" name="" />
-								</label>
-								<label>
-									<strong>Company name</strong>
-									<input type="text" placeholder="acme corp" name="" />
-								</label>
-								<label>
-									<strong>Company size</strong>
-									<select name="">
-										<option value="" disabled selected>
-											Select a range of employees
-										</option>
-										<option value="100">1 - 100</option>
-										<option value="200">101 - 200</option>
-										<option value="300">201 - 300</option>
-										<option value="400">Over 300</option>
-									</select>
-								</label>
-								<label>
-									<strong>Message</strong>
-									<textarea placeholder="Tell your project or anything else we can help"></textarea>
-								</label>
-								<Button
-									text="Send Message"
-									type="flat arrow submit"
-									color="white"
-								/>
-							</form>
+					<div className="container">
+						<div className="hero__wrapper">
+							<div className="hero__content">
+								<p className="hero__name">Contact Us</p>
+								<h1 className="hero__title">We’d love to hear from you</h1>
+								<p className="hero__description">
+									If you have any questions about Paxdorm please contact us in
+									form beside and we will respond immediately.
+								</p>
+							</div>
+							<div className="form__box">
+								<form>
+									<label>
+										<strong>Full name</strong>
+										<input type="text" placeholder="John Doe" name="" />
+									</label>
+									<label>
+										<strong>Email</strong>
+										<input type="text" placeholder="jondoe@email.com" name="" />
+									</label>
+									<label>
+										<strong>Company name</strong>
+										<input type="text" placeholder="acme corp" name="" />
+									</label>
+									<label>
+										<strong>Company size</strong>
+										<select name="">
+											<option value="" disabled selected>
+												Select a range of employees
+											</option>
+											<option value="100">1 - 100</option>
+											<option value="200">101 - 200</option>
+											<option value="300">201 - 300</option>
+											<option value="400">Over 300</option>
+										</select>
+									</label>
+									<label>
+										<strong>Message</strong>
+										<textarea placeholder="Tell your project or anything else we can help"></textarea>
+									</label>
+									<Button
+										text="Send Message"
+										type="flat arrow submit"
+										color="white"
+									/>
+								</form>
+							</div>
 						</div>
 					</div>
 					<div className="bottom_triangle bottom_triangle--white"></div>
 				</section>
 				<section className="help">
-					<div className="container ">
+					<div className="container">
 						<span className="help-name">GET IN TOUCH</span>
 						<h2 className="help-title">Our teams are here to help</h2>
-						<div className="help__wrapper">
+						<div className="help__wrapper container">
 							<div className="help__box">
 								<em className="help__box-icon"></em>
 								<strong className="help__box-title">
