82fa0b9f86757a1057af910259943d55004659dd
Miri Kim <miri.kim@paxform.com>
2023-01-18 14:45:00 +1100

    company changed SCSS

--- src/pages/Pricing/Pricing.jsx
+++ src/pages/Pricing/Pricing.jsx
@@ -24,71 +24,78 @@ export default class Pricing extends Component {
 								</div>
 							</div>
 							<div className="hero__plans">
-								<div className="hero_plans__free">
-									<i className="hero__plans-icon">flags</i>
-									<h2 className="hero__plans-name">Free</h2>
-									<p className="hero__plans-description">
-										A simple way to get started for you new to passfolio.
-									</p>
-									<strong className="hero__plans-cost">
-										$0<em>/forever</em>
-									</strong>
-									<button>Get started</button>
-									<dl className="hero__plans-benefits">
-										<dt>What's Included</dt>
-										<dd>Unlimited data</dd>
-										<dd>5 Person saved</dd>
-										<dd>
-											A Simple way to get started for you new to Passfolio
-										</dd>
-									</dl>
+								<div className="hero__plans__free">
+									<div className="hero__plans__free__inner">
+										<i className="hero__plans-icon">flags</i>
+										<h2 className="hero__plans-name">Free</h2>
+										<p className="hero__plans-description">
+											A simple way to get started for you new to passfolio.
+										</p>
+										<strong className="hero__plans-cost">
+											$0<em>/forever</em>
+										</strong>
+										<button>Get started</button>
+										<dl className="hero__plans-benefits">
+											<dt>What's Included</dt>
+											<dd>Unlimited data</dd>
+											<dd>5 Person saved</dd>
+											<dd>
+												A Simple way to get started for you new to Passfolio
+											</dd>
+										</dl>
+									</div>
 								</div>
-								<div className="hero_plans__Premium">
-									<em>Most Popular</em>
-									<i className="hero__plans-icon">/Star</i>
-									<h2 className="hero__plans-name">Freemium</h2>
-									<p className="hero__plans-description">
-										Best for freelance coders & designers who need.
-									</p>
-									<strong className="hero__plans-cost">
-										$50<em>/forever</em>
-									</strong>
-									<button>Get started</button>
-									<dl className="hero__plans-benefits">
-										<dt>What's Included</dt>
-										<dd>Unlimited data</dd>
-										<dd>5 Person saved</dd>
-										<dd>
-											A Simple way to get started for you new to Passfolio
-										</dd>
-										<dd>
-											A Simple way to get started for you new to Passfolio
-										</dd>
-									</dl>
+								<div className="hero__plans__premium">
+									<div className="hero__plans__premium__inner">
+										<em>Most Popular</em>
+										<i className="hero__plans-icon">/Star</i>
+										<h2 className="hero__plans-name">Freemium</h2>
+										<p className="hero__plans-description">
+											Best for freelance coders & designers who need.
+										</p>
+										<strong className="hero__plans-cost">
+											$50<em>/forever</em>
+										</strong>
+										<button>Get started</button>
+										<dl className="hero__plans-benefits">
+											<dt>What's Included</dt>
+											<dd>Unlimited data</dd>
+											<dd>5 Person saved</dd>
+											<dd>
+												A Simple way to get started for you new to Passfolio
+											</dd>
+											<dd>
+												A Simple way to get started for you new to Passfolio
+											</dd>
+										</dl>
+									</div>
 								</div>
-								<div className="hero_plans__Family">
-									<i className="hero__plans-icon">Family</i>
-									<h2 className="hero__plans-name">Family</h2>
-									<p className="hero__plans-description">
-										A simple way to get started for you new to passfolio.
-									</p>
-									<strong className="hero__plans-cost">Custom</strong>
-									<button>Get started</button>
-									<dl className="hero__plans-benefits">
-										<dt>What's Included</dt>
-										<dd>Unlimited data</dd>
-										<dd>5 Person saved</dd>
-										<dd>
-											A Simple way to get started for you new to Passfolio
-										</dd>
-										<dd>
-											A Simple way to get started for you new to Passfolio
-										</dd>
-									</dl>
+								<div className="hero__plans__family">
+									<div className="hero__plans__family__inner">
+										<i className="hero__plans-icon">Family</i>
+										<h2 className="hero__plans-name">Family</h2>
+										<p className="hero__plans-description">
+											A simple way to get started for you new to passfolio.
+										</p>
+										<strong className="hero__plans-cost">Custom</strong>
+										<button>Get started</button>
+										<dl className="hero__plans-benefits">
+											<dt>What's Included</dt>
+											<dd>Unlimited data</dd>
+											<dd>5 Person saved</dd>
+											<dd>
+												A Simple way to get started for you new to Passfolio
+											</dd>
+											<dd>
+												A Simple way to get started for you new to Passfolio
+											</dd>
+										</dl>
+									</div>
 								</div>
 							</div>
 						</div>
 					</div>
+					<div className="bottom_triangle bottom_triangle--white"></div>
 				</section>
 				<section className="data_credit">
 					<div className="container">
@@ -120,12 +127,13 @@ export default class Pricing extends Component {
 							</li>
 						</ul>
 					</div>
-					<div className="bottom_triangle bottom_triangle--white"></div>
 				</section>
 				<section className="compare_plans">
+					<div className="container"></div>
 					<div className="bottom_triangle bottom_triangle--white"></div>
 				</section>
 				<section className="frequently_ask">
+					<div className="container"></div>
 					<div className="bottom_triangle bottom_triangle--white"></div>
 				</section>
 				<ReadyGet />
