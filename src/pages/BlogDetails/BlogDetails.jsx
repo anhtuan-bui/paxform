7d400d88cc1bdb424d93bce8b611cee8e92dcbd6
Mahmoud Elkazzaz <mahmoud.elkazzaz@paxform.com>
2023-02-23 13:00:20 +1100

    Fixing the height of the left section image for tablet and bigger screen sizes

--- src/pages/BlogDetails/BlogDetails.jsx
+++ src/pages/BlogDetails/BlogDetails.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react'
+import React, { useState } from 'react'
+import { useEffect } from 'react';
 import './BlogDetails.scss';
 import RelatedCard from '../../components/RelatedCard/RelatedCard';
 import SectionTriangleRight from "../../components/SectionTriangleRight/SectionTriangleRight";
@@ -8,11 +9,28 @@ import LatestBlogs from "../../components/LatestBlogs/LatestBlogs"
 import CardResource1 from "../../assets/images/card-resource-1.png";
 import CardResource3 from "../../assets/images/card-resource-3.png";
 import Card2 from "../../assets/images/card2.png";
-import VideoPoster from "../../assets/images/company-video.png";
 import Video from "../../assets/videos/rick.mp4"
 import SampleAuthorAvatar from "../../assets/images/sample-author-avatar.png";
 
 const BlogDetails = () => {
+
+    let [smallerImage, setSmallerImage] = useState("");
+    const smallImage = () => {
+        if (window.innerWidth >= 768) {
+            setSmallerImage("related_card__image--smaller")
+        } else {
+            setSmallerImage("");
+        }
+    }
+
+    useEffect(() => {
+        smallImage()
+        window.addEventListener('resize', smallImage);
+        return () => {
+            window.removeEventListener("resize", smallImage)
+        }
+    });
+
     return (
         <main>
             <div className="container hero" background="light">
@@ -46,7 +64,7 @@ const BlogDetails = () => {
                             It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
                             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus .
                         </div>
-                        <video className="article__video" src={Video} poster={VideoPoster} controls />
+                        <video className="article__video" src={Video} controls />
                         <div className="article__snippet">
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tristique, lorem in accumsan molestie, tellus quam lobortis arcu, in lacinia metus magna vitae nunc. Quisque scelerisque turpis dolor, id auctor nulla condimentum id. Maecenas luctus cursus tortor, in vestibulum nisl congue sit amet. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
                             Aenean ornare magna at urna porttitor, eget dapibus quam convallis. Nam tincidunt pellentesque pharetra. Proin tincidunt velit non mauris semper porttitor. Nam nec sollicitudin leo. Curabitur tempor sed quam eget hendrerit. In consequat dolor at dui pharetra bibendum. Etiam ullamcorper feugiat mauris id pulvinar. Donec maximus lectus mauris, sit amet placerat mauris gravida sit amet. Nam elementum turpis a mi tincidunt, nec accumsan ipsum bibendum. Phasellus malesuada viverra quam eu laoreet. Curabitur non aliquet odio, et malesuada lorem.
@@ -58,7 +76,7 @@ const BlogDetails = () => {
                         <p className="recommended__title section_name"></p>
                         <div className="recommended_container">
                             <div className="recommended__article">
-                                <RelatedCard image={CardResource3} title="First Heading" description readLink />
+                                <RelatedCard className={smallerImage} image={CardResource3} title="First Heading" description readLink />
                             </div>
                             <div className="recommended__article">   
                                 <RelatedCard image={CardResource1} title="Third Heading" description readLink />    
