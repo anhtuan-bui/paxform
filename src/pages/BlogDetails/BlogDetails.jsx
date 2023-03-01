287a4c37a86e842635ed16a9ae9ec2ddbaf3e59d
Mahmoud Elkazzaz <mahmoud.elkazzaz@paxform.com>
2023-03-01 12:40:57 +1100

    Changing Video to a YouTube Video

--- src/pages/BlogDetails/BlogDetails.jsx
+++ src/pages/BlogDetails/BlogDetails.jsx
@@ -9,7 +9,7 @@ import LatestBlogs from "../../components/LatestBlogs/LatestBlogs"
 import CardResource1 from "../../assets/images/card-resource-1.png";
 import CardResource3 from "../../assets/images/card-resource-3.png";
 import Card2 from "../../assets/images/card2.png";
-import Video from "../../assets/videos/rick.mp4"
+// import Video from "https://www.youtube.com/embed/tgbNymZ7vqY";
 import SampleAuthorAvatar from "../../assets/images/sample-author-avatar.png";
 
 const BlogDetails = () => {
@@ -64,7 +64,9 @@ const BlogDetails = () => {
                             It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
                             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus .
                         </div>
-                        <video className="article__video" src={Video} controls />
+                        <iframe title="Article Video" className="article__video"
+                            src="https://www.youtube.com/embed/tgbNymZ7vqY">
+                        </iframe>
                         <div className="article__snippet">
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tristique, lorem in accumsan molestie, tellus quam lobortis arcu, in lacinia metus magna vitae nunc. Quisque scelerisque turpis dolor, id auctor nulla condimentum id. Maecenas luctus cursus tortor, in vestibulum nisl congue sit amet. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
                             Aenean ornare magna at urna porttitor, eget dapibus quam convallis. Nam tincidunt pellentesque pharetra. Proin tincidunt velit non mauris semper porttitor. Nam nec sollicitudin leo. Curabitur tempor sed quam eget hendrerit. In consequat dolor at dui pharetra bibendum. Etiam ullamcorper feugiat mauris id pulvinar. Donec maximus lectus mauris, sit amet placerat mauris gravida sit amet. Nam elementum turpis a mi tincidunt, nec accumsan ipsum bibendum. Phasellus malesuada viverra quam eu laoreet. Curabitur non aliquet odio, et malesuada lorem.
