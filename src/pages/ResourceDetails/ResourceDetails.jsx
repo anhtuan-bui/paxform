54e628a34575473460737d72430e9b42dbf1c7e2
Bishoy <BishoyMichail334@gmail.com>
2023-03-06 10:36:29 +1100

    Resource details update & FAQV1 creation

--- src/pages/ResourceDetails/ResourceDetails.jsx
+++ src/pages/ResourceDetails/ResourceDetails.jsx
@@ -20,23 +20,101 @@ export default class ResourceDetails extends Component {
         "Simple data form"
     ]
 
+
+    dropdownBtn = document.getElementsByClassName("dropdown__dropbtn");
+    dropdownMenu = document.getElementsByClassName("dropdown__content");
+    toggleArrow = document.getElementsByClassName("arrow-down");
+
+    // document.getElementsByClassName("dropdown__dropbtn").addEventListener("click", function(e) {
+
+    // });
+    // dropdownClick () {
+    //     // 
+    //     dropdownBtn.addEventListener("click", functon(e) {
+    //         dropdownMenu.classList.toggle
+    //     })
+    // }
+
     constructor(props) {
         super(props);
 
-        this.state = {dropdownValue: this.dropdownValue}
+
+        this.state = { dropdownValue: this.dropdownValue }
+        this.handleScroll = this.handleScroll.bind(this);
+        //this.testFunction = this.testFunction.bind(this);
+        //this.color = this.color.bind(this);
+        //this.outcolor = this.outcolor.bind(this);
+    }
+
+    componentDidMount() {
+        // document.addEventListener("scroll", this.handleScroll); 
+        this.testFunction();
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener("scroll", this.handleScroll);
+
     }
 
     handleDropdownOptionClick(e) {
         const optionValue = e.target.getAttribute("value");
-        this.setState({dropdownValue: optionValue });
+        this.setState({ dropdownValue: optionValue });
     }
 
-   /*  $(window).scroll(function(){
-        if($window).scrollTop() > 280) {
-            this.$('.scrolling').addClass('end');
-            this.$()
+    handleScroll() {
+        const dropdown = document.querySelector(".dropdown-table")
+
+        if (window.scrollY >= 236) {
+            dropdown.classList.add("dropdown-table--active");
+        } else {
+            dropdown.classList.remove("dropdown-table--active");
         }
-    }) */
+        this.setState(this.app);
+    }
+
+    testFunction() {
+        // document.getElementsByClassName('first')[0].onMouseOver = function () {
+        //     document.getElementByClassName('outer-box')[0].style="background: black"
+        // }
+
+        document.addEventListener('scroll', () => {
+            //const y = 200;
+
+            const headings = document.querySelectorAll('h2');
+
+            headings.forEach((heading, i) => {
+                const top = heading.getClientRects()[0].top;
+                if (top < 200 && top > 100) {
+                    const listItems = document.querySelectorAll('.list-item');
+                    listItems[i].classList.add("list-item--active");
+                    listItems.forEach((item, j) => {
+                        if (j !== i) {
+                            item.classList.remove("list-item--active");
+                        }
+                    })
+                }
+
+            })
+
+            // const h2Top = document.querySelector('h2').getClientRects()[0].top;
+
+            // if (h2Top < 200) {
+            //    const listItems = document.querySelectorAll('.list-item-text');
+            //    listItems[0].classList.add("list-item--active");
+
+            // }
+        })
+        document.getElementsByClassName('first')[0].onMouseOut = function () {
+            document.getElementsByClassName('outer-box')[0].style = "background: green"
+        }
+    }
+    color() {
+        document.getElementById("outer").style.background = "black";
+    }
+    outcolor() {
+        document.getElementById("outer").style.background = "green";
+    }
+
 
     render() {
         return (
@@ -46,31 +124,52 @@ export default class ResourceDetails extends Component {
                 </section>
                 <div className="container">
                     <div className="wrapper">
-                        <section className="content__info">
-                            <p className="content__info-caseStudy">IN THIS CASE STUDY</p>
-                            {/* <div className="dropdown-select">
-                                <select className="dropdown">
-                                    <div className="dropdown-content">
-                                        <option><a href="#typesetting">Changing the process</a></option>
-                                        <option>Growing Quickly</option>
-                                        <option>Needing a soltution</option>
-                                        <option>Simple data form</option>
+                        <div className="content__info">
+                            <div className="content__wrapper">
+
+                                {/* START OF SIDE TABLE STICKY */}
+                                <p className="content__info-caseStudy">IN THIS CASE STUDY</p>
+                                <div className="table-wrapper">
+                                    <div className="dropdown-table">
+                                        <ul>
+                                            <li className="list-item">
+                                                <div className="blue-bar"></div>
+                                                <a className="list-item-text" href={`#heading20`} name="heading20" >Changing the process</a>
+                                            </li>
+                                            <li className="list-item">
+                                                <div className="blue-bar"></div>
+                                                <a className="list-item-text" href={`#heading29`} name="heading29">Growing quickly</a>
+                                            </li>
+                                            <li className="list-item">
+                                                <div className="blue-bar"></div>
+                                                <a className="list-item-text" href={`#heading22`}>Needing a solution</a>
+                                            </li>
+                                            <li className="list-item">
+                                                <div className="blue-bar"></div>
+                                                <a className="list-item-text" href={`#heading23`}>Simple data form</a>
+                                            </li>
+                                        </ul>
                                     </div>
-                                </select>
-                            </div> */}
+                                </div>
+                            </div>
+                            <div className="empty-space"></div>
+
+                            {/* END OF SIDE TABLE STICKY */}
+                            {/* START OF DROPDOWN MENU   */}
+
                             <div className="dropdown">
-                                <div className="whatever">
+                                <div className="dropdown__button-wrapper">
                                     <div className="blue-bar"></div>
-                                    <button className="dropbtn">
+                                    <button className="dropdown__dropbtn">
                                         <div className="button_div">
                                             <div className="blue-bar"></div>
                                             {this.state.dropdownValue}
                                         </div>
-                                        <img src={arrowDown} alt="arrow" />
+                                        <img className="arrow-down" src={arrowDown} alt="arrow" />
                                     </button>
 
                                 </div>
-                                <div className="dropdown-content">
+                                <div className="dropdown__content">
                                     {
                                         this.dropdown.map((option, index) => (
                                             <div className="list-item" key={index} >
@@ -79,32 +178,15 @@ export default class ResourceDetails extends Component {
                                             </div>
                                         ))
                                     }
+                                </div>
+                            </div>
+                            {/* </div>                     */}
+                            {/* <div className="empty-space"></div>  */}
 
-                                </div>
-                            </div>
-                            <div className="table-wrapper">
-                                <div className="dropdown-table">
-                                    <ul>
-                                        <li className="list-item">
-                                            <div className="blue-bar"></div>
-                                            <div className="list-item-text">Changing the process</div>
-                                        </li>
-                                        <li className="list-item">
-                                            <div className="blue-bar"></div>
-                                            <div className="list-item-text">Growing quickly</div>
-                                        </li>
-                                        <li className="list-item">
-                                            <div className="blue-bar"></div>
-                                            <div className="list-item-text">Needing a solution</div>
-                                        </li>
-                                        <li className="list-item">
-                                            <div className="blue-bar"></div>
-                                            <div className="list-item-text">Simple data form</div>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </section>
+                            {/* END OF DROPDOWN MENU   */}
+
+                        </div>
+                        {/* </div> */}
                         <section className="article">
                             <div className="article__heading">
                                 <p className="article-date">DECEMBER 25, 2021</p>
@@ -119,24 +201,17 @@ export default class ResourceDetails extends Component {
                                     packages and web page editors now use Lorem Ipsum as their default model text, anda  search for 'lorem ipsum'
                                     will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by
                                     accident, sometimes on purpose (injected humour and the like).</p>
-                                <br />
-                                <br />
                                 <p id="heading21">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
                                     standard dummy text ever since the 1500's, when an unkown printer took a galley of type and scrambled it to make
                                     a type specimen book. it has survived not only five centuries, but also the leap into electronic typesetting,
                                     remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
                                     Lorem Ipsum passages, and more recentyl with desktop publishing software like Aldus.</p>
-                                <br />
-                                <br />
                                 <img className="main_img" src={dentistImage2} alt="dentistImage2" />
-                                <br />
-                                <br />
                                 <p id="heading22">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tristique, lorem in accusmsan molestie, tellus
                                     quam lobortis arcu, in lacinia metus magna vitae nunc. Quisque scelerisqu turpis dolor, id auctor nulla condimentum
                                     id. Maecenas luctus cursus tortor, in vetibulum nisl congue sit amet. Orci varius natoque penatibus et magnis dis
                                     parturient montes, nasceture ridiculus mus.</p>
-                                {/* <br />
-                                <br /> */}
+                                <h2 id="heading29">Heading 2 Lorem Ipsum has been the industry's standard dummy the 1500s</h2>
                                 <p id="heading23">Aenean ornare magna at urna porttitor, eget dapibus quam convallis. Nam tincidunt pellentesque pharetra. Proin
                                     tincidunt velit non mauris semper porttitor. Nam nec sollicitudin leo. Curabitur tempor sed quam eget hendrerit.
                                     In consequat dolor at dui pharetra bibendum. Etiam ullamcorper feugiat mauris id pulvinar. Donec maximus lectus
@@ -145,11 +220,19 @@ export default class ResourceDetails extends Component {
                             </div>
                         </section>
                         <hr className="black-bar" />
-                        <SocialIcons className="social-margin-update" />
+                        <SocialIcons className="social-margin-change" />
                     </div>
                 </div>
-                <br />
-                <br />
+
+                <div className="icons">
+                    <div className="first"></div>
+                </div>
+                
+
+                {/* <div className="icons">
+                    <div className="first" onmouseover="color()" onmouseout="outcolor()"></div>
+                </div>
+                <div className="outer-box" id="outer"></div> */}
 
                 <SectionTriangleRight variant="light-blue" />
 
