2d7a0bce1501b1722f456319d8e1a0f51a59cbe4
Mahmoud Elkazzaz <mahmoud.elkazzaz@paxform.com>
2023-03-30 14:01:08 +1100

    Modifying RelatedCard Component

--- src/components/RelatedCard/RelatedCard.jsx
+++ src/components/RelatedCard/RelatedCard.jsx
@@ -3,43 +3,52 @@ import "./RelatedCard.scss";
 
 const RelatedCard = (props) => {
 
-    return (
-      <div className="related_card">
-        <div className="related_card__top">
-          {props.image ? (
-            <div className={`${props.className} related_card__image`}>
-              <img src={props.image} alt="first story" />
-            </div>
-          ) : (
-            ""
-          )}
+  return (
+    <div className="related_card">
+      <div className="related_card__top">
+        {props.image ? (
+          <div
+            className={`${
+              props.className ? props.className : ""
+            } related_card__image`}
+          >
+            <img src={props.image} alt="first story" />
+          </div>
+        ) : (
+          ""
+        )}
 
-          {props.category ? (
-            <p className="related_card__category">{props.category}</p>
-          ) : ( 
-            ""
-          )}
+        {props.category ? (
+          <p className="related_card__category">{props.category}</p>
+        ) : (
+          ""
+        )}
 
-          {props.title ? (
-            <h2 className="related_card__title">{props.title}</h2>
-          ) : (
-            ""
-          )}
-          {props.description ? (
-            <p className="related_card__description">
-              There are many variations of passages of Lorem Ipsum available,
-              but the majority have suffered alteration in some form, by
-              injected humour...
-            </p>
-          ) : (
-            ""
-          )}
-        </div>
-        <div className="related_card__bottom">
-          {props.readLink ? <ReadArticle to="related" /> : ""}
-        </div>
+        {props.title ? (
+          <h2
+            className="related_card__title"
+            dangerouslySetInnerHTML={{ __html: props.title }}
+          ></h2>
+        ) : (
+          ""
+        )}
+
+        {props.description ? (
+          <div
+            className="related_card__description"
+            dangerouslySetInnerHTML={{ __html: props.description }}
+          ></div>
+        ) : (
+          ""
+        )}
       </div>
-    );
+      <div className="related_card__bottom">
+        {props.readLink ? <ReadArticle to="related" /> : ""}
+      </div>
+    </div>
+  );
 }
 
 export default RelatedCard;
+
+
