f5a55df40133c6d6ed73cd0927251a438f43a81a
Mahmoud Elkazzaz <mahmoud.elkazzaz@paxform.com>
2023-03-31 12:13:31 +1100

    ReadArticle scroll to top when clicked

--- src/components/ReadArticle/ReadArticle.jsx
+++ src/components/ReadArticle/ReadArticle.jsx
@@ -4,8 +4,12 @@ import { Link } from "react-router-dom";
 import { ReactComponent as ArrowRight } from "../../assets/images/arrow-right.svg";
 export default class ReadArticle extends Component {
   render() {
+    // When the Read Article link is clicked on the related card the page will scroll up to the top
+    const scrollTop = () => {
+      window.scrollTo(0, 0);
+    }
     return (
-      <Link className="read_article" to={this.props.to}>
+      <Link className="read_article" to={this.props.to} onClick={scrollTop}>
         <span>Read Article</span>
         <ArrowRight className="read_article__arrow"/>
       </Link>
