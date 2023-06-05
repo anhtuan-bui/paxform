493de187f5591a536dbc4b38d902b81284c21fef
anhtuan-bui <anhtuan.bui@paxform.com>
2023-06-05 14:32:35 +1000

    change default language to en us

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -66,10 +66,11 @@ const socialMedias = [
 export default function Footer() {
   const [selector, setSelector] = useState(false);
   const [overlayOpened, setOverlayOpened] = useState(false);
-  const [shortName, setShortName] = useState("au");
+  const [shortName, setShortName] = useState("us");
   const [languageFullName, setLanguageFullName] = useState(
-    "English - Australia"
+    "English - United States"
   );
+  const defaultLang = "EN_US";
   const { loading, data } = useQuery(GET_LANGUAGES);
 
   const languages = !loading ? data?.languages : Array.from({ length: 3 });
@@ -411,6 +412,8 @@ export default function Footer() {
         (language) => language?.locale?.toLowerCase() === lang.toLowerCase()
       );
       setLanguageFullName(language?.name);
+    } else {
+      localStorage.setItem("lang", defaultLang);
     }
 
     if (notFound) {
@@ -541,7 +544,7 @@ const SelectorOption = ({ language, onClick }) => {
       onClick={(e) => onClick(e)}
       locale={language?.locale?.toUpperCase()}
       shortname={shortName}
-      fullname={language?.name }
+      fullname={language?.name}
     >
       <span className={`fi fi-${shortName}`}></span> {language?.name}
     </div>
