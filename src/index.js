af356570d929d8ff3b31387de36d83434498052f
Anh Tuan <anhtuan128@gmail.com>
2023-09-13 21:59:11 +1000

    add ga

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import "./index.scss";
 import "./styles/wp-style.css";
 import App from "./App";
 import "./configurations/i18n";
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-YQJETRELZP");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
